ccf81970014135ec10bde6a04b4c27a3aa670a44
Dawid Jasiński <ambas90@gmail.com>
2024-01-27 12:25:51 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,7 @@ import ContactList from './ContactList';
 
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -45,6 +40,27 @@ export default class App extends Component {
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
+
+  saveContacts = () => {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  };
+
+  loadContacts = () => {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (savedContacts.length > 0) {
+      this.setState({
+        contacts: savedContacts,
+      });
+    }
+  };
+
+  componentDidMount() {
+    this.loadContacts();
+  }
+
+  componentDidUpdate() {
+    this.saveContacts();
+  }
 
   render() {
     const { filter } = this.state;
